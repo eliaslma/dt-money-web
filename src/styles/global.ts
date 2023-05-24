7107dfe3a2e4019c960cb9d694924906acb1955d
Elias Lima <sci.eliaslima@gmail.com>
2023-05-24 01:14:26 -0300

    feat: create highlight card

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -42,6 +42,34 @@ export const GlobalStyles = createGlobalStyle`
         font-weight: 700;
     }
 
+    .react-modal-overlay{
+        background-color: rgba(0,0,0,0.5);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .react-modal-content{
+        width: 100%;
+        max-width: 576px;
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.35rem;
+        background-color: var(--background);
+    }
+
+    .react-modal-close{
+        background-color: transparent;
+        position: absolute;
+        border: 0;
+        top: 1.5rem;
+        right: 1.5rem;
+    }
+
     html {
         @media (max-width: 1080px){
             font-size: 93.75%; //15px
